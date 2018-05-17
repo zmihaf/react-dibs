e6908b5b7c4835b5ba390da2391116175da18bf7
Azmi Hafizha <azmi.hafizha@outlook.com>
2018-05-17 16:25:23 +0700

    remove reconnect option

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -6,7 +6,7 @@ class DibsClientComponent extends Component {
     pageVisitorList: [],
   }
 
-  socket = io(this.props.server, { secure: true, reconnect: true, rejectUnauthorized : false });
+  socket = io(this.props.server, { secure: true, rejectUnauthorized : false });
 
   componentDidMount() {
     this.socket.emit('PAGE_VISIT', window.location.href, this.props.username, Date.now());
